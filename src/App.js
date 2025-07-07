56307dcf1bc6d44ccf962644d338b36daa32669d
cistej <joseignacio.cisternas@fsanlazaro.cl>
2025-07-06 20:05:17 -0400

    feat: Implementar nuevo diseño y funcionalidad de exportar version 2

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ import {
 // --- CONFIGURACIÓN DE FIREBASE ---
 // REEMPLAZA ESTOS VALORES CON LA CONFIGURACIÓN DE TU PROYECTO DE FIREBASE
 const firebaseConfig = {
-    aapiKey: "AIzaSyDaKoae1hfnDPJlWLiM64fDq1-hPRivn44",
+    apiKey: "AIzaSyDaKoae1hfnDPJlWLiM64fDq1-hPRivn44",
   authDomain: "cashing-out-app.firebaseapp.com",
   projectId: "cashing-out-app",
   storageBucket: "cashing-out-app.firebasestorage.app",
